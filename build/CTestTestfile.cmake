c306bcc7f861faf94d5313c19cc2a28d61c098bf
RexCoins <akrantor2005az@gmail.com>
2026-04-26 19:17:44 +0600

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /data/data/com.termux/files/home/RexCoin
+# Build directory: /data/data/com.termux/files/home/RexCoin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(core_test "/data/data/com.termux/files/home/RexCoin/build/core_test")
+set_tests_properties(core_test PROPERTIES  _BACKTRACE_TRIPLES "/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;99;add_test;/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;0;")
+add_test(crypto_test "/data/data/com.termux/files/home/RexCoin/build/crypto_test")
+set_tests_properties(crypto_test PROPERTIES  _BACKTRACE_TRIPLES "/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;99;add_test;/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;0;")
+add_test(network_test "/data/data/com.termux/files/home/RexCoin/build/network_test")
+set_tests_properties(network_test PROPERTIES  _BACKTRACE_TRIPLES "/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;99;add_test;/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;0;")
+add_test(vm_test "/data/data/com.termux/files/home/RexCoin/build/vm_test")
+set_tests_properties(vm_test PROPERTIES  _BACKTRACE_TRIPLES "/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;99;add_test;/data/data/com.termux/files/home/RexCoin/CMakeLists.txt;0;")
